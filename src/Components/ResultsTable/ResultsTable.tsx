a47616a94eac62e354d23e608f91e2d17a95a4fc
Jong Kim <kim.jongjhun@gmail.com>
2024-02-20 23:14:39 -0500

    Started on laying out app.

--- src/Components/ResultsTable/ResultsTable.tsx
+++ src/Components/ResultsTable/ResultsTable.tsx
@@ -1,8 +1,27 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
 const ResultsTable = () => {
   return (
-    <>
-      <>results table</>
-    </>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
