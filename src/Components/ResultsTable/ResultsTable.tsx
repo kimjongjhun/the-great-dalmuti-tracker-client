440ddcf9ec09a37bd0c82b6e792b2f898fceeba3
Jong Kim <kim.jongjhun@gmail.com>
2024-02-22 16:53:53 -0500

    Added momentjs, got getAllRounds from db working.

--- src/Components/ResultsTable/ResultsTable.tsx
+++ src/Components/ResultsTable/ResultsTable.tsx
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -6,20 +8,74 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-const ResultsTable = () => {
+const ResultsTable = ({ rounds }) => {
+  const { results, numberOfPlayers } = rounds;
+
+  const RenderPlayerNumber = ({ index }: { index: number }) => {
+    let label;
+
+    if (index === 0) {
+      label = "Dalmuti";
+    } else if (index === numberOfPlayers - 1) {
+      label = "Serf";
+    } else {
+      label = index + 1;
+    }
+
+    return label;
+  };
+
+  const RenderTableHead = () => {
+    const dataCells = [];
+    for (let x = 0; x < numberOfPlayers; x++) {
+      dataCells.push(
+        <TableCell align={"center"}>
+          <RenderPlayerNumber index={x} />
+        </TableCell>
+      );
+    }
+
+    return (
+      <TableHead>
+        <TableRow>
+          <TableCell align={"center"}>Date</TableCell>
+          {dataCells}
+        </TableRow>
+      </TableHead>
+    );
+  };
+
+  const RenderTableBody = () => {
+    return (
+      <TableBody>
+        {results.map((round, index) => {
+          const { date, playerOrder } = round;
+          const dataCells = [];
+
+          const roundDate = moment(date).format("MM/DD/YYYY").toString();
+
+          for (let x = 0; x < playerOrder.length; x++) {
+            dataCells.push(
+              <TableCell align={"center"}>{playerOrder[x]}</TableCell>
+            );
+          }
+
+          return (
+            <TableRow key={index}>
+              <TableCell align={"center"}>{roundDate}</TableCell>
+              {dataCells}
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    );
+  };
+
   return (
     <TableContainer component={Paper}>
       <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell></TableCell>
-          </TableRow>
-        </TableBody>
+        {<RenderTableHead />}
+        {<RenderTableBody />}
       </Table>
     </TableContainer>
   );
